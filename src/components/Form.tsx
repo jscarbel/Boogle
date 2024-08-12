85435fb9d86323aa34c02ae400399b3f7c23abef
jaimie <jaimiescarbel@gmail.com>
2024-08-12 18:11:11 -0400

    added better error handling

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -11,22 +11,34 @@ export const Form = ({
 }) => {
   const [userName, setUserName] = useState<string>("");
   const [error, setError] = useState<string>("");
+  const [loading, setLoading] = useState<boolean>(false);
 
   const handleSubmit = async (event: React.FormEvent) => {
     event.preventDefault();
-    if (error) return;
+    if (error || loading) return;
+    setLoading(true);
     const baseUrl = process.env.REACT_APP_BACKEND_BASE_URL;
     if (typeof baseUrl !== "string") return;
-    await fetch(`${baseUrl}/score`, {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({ userName, score: totalScore, wordCount }),
-    });
+    try {
+      const response = await fetch(`${baseUrl}/score`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ userName, score: totalScore, wordCount }),
+      });
 
-    setUserName("");
-    onFormSubmit();
+      if (response.ok) {
+        setLoading(false);
+        onFormSubmit();
+      } else {
+        const failedResult = await response.json();
+        console.error(failedResult);
+      }
+    } catch (e) {
+      console.error(e);
+    }
+    setLoading(false);
   };
 
   const handleInputChange = (val: string) => {
@@ -49,7 +61,13 @@ export const Form = ({
           onChange={(e) => handleInputChange(e.target.value)}
         />
       </label>
-      <input type="submit" />
+      <input
+        type="submit"
+        className={loading ? "disabled" : ""}
+        disabled={loading}
+      >
+        {loading ? "Submitting..." : "submit"}
+      </input>
     </form>
   );
 };
